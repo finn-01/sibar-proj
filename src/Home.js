741d3ca9855db4dd913e5cb7b4a0bf12c8a37e61
finn-01 <nguyensam576@gmail.com>
2022-01-15 11:21:40 +0700

    home.js

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
+
+import { useGlobalContext } from "./context";
 import { FaBars } from "react-icons/fa";
 
 const Home = () => {
-	return <div> Hello</div>;
+	const { openSidebar, openModal } = useGlobalContext();
+
+	return (
+		<main>
+			<button onClick={openSidebar} className="sidebar-toggle">
+				<FaBars />
+			</button>
+			<button onClick={openModal} className="btn">
+				show modal
+			</button>
+		</main>
+	);
 };
 
 export default Home;
